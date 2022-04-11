f2e6fe50c4e9b5bea4f9bca9cc975ad7d2028c6e
wsr5 <wsr5@K218PCREZERV>
2022-04-11 11:44:55 +0300

    Добавьте файлы проекта.

---
+++ ExpressionLambdas2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpressionLambdas2
+{
+
+    public delegate string StrDelegate(string s, int x);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StrDelegate sD = (string str, int a) => str + Convert.ToString(a);
+            Console.WriteLine(sD("abc", 123));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lambdas/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambdas
+{
+    public delegate void HelloDelegate();
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloDelegate hD = () => Console.WriteLine("Hello!");
+
+            hD();
+
+            Console.ReadKey();
+        }
+    }
+    }
+
